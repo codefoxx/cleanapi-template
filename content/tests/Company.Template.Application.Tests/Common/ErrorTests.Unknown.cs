4c6500e837ecac3b45e9f6b3d4766a87416d536b
codefoxx <65673235+codefoxx@users.noreply.github.com>
2026-05-22 09:27:13 +0200

    refactor: add error unknown partial

---
+++ content/tests/Company.Template.Application.Tests/Common/ErrorTests.Unknown.cs
@@ -0,0 +1,18 @@
+using Company.Template.Application.Common;
+
+namespace Company.Template.Application.Tests.Common;
+
+public sealed partial class ErrorTests
+{
+    [Fact]
+    public void Unknown_WithCodeAndMessage_ReturnsUnknownError()
+    {
+        // Act
+        Error error = Error.Unknown(ErrorCode.Create("unexpected_error"), "Unexpected error.");
+
+        // Assert
+        error.Type.ShouldBe(ErrorType.Unknown);
+        error.Code.Value.ShouldBe("unexpected_error");
+        error.Message.ShouldBe("Unexpected error.");
+    }
+}
